39a270458850a1d0a1e3c4fb1e1e6cb8c3b7f8f6
Hae1385 <206494390+Hae1385@users.noreply.github.com>
2025-06-12 17:09:43 +0900

    ✨Add : 플레이어 임시데이터 추가
    플레이어의 스텟이 보관될 임시 데이터를 추가하였습니다

---
+++ Assets/Scripts/PlayerData.cs
@@ -0,0 +1,24 @@
+﻿
+[System.Serializable]
+public  class  PlayerData //임시입니다 스텟레벨을 가져올예정
+{
+    public int Stage;
+    public int Gold;
+    public int Attack;
+    public int Critical;
+    public int CriticalDmg;
+    public int BonusGold;
+
+    public void RefreshData(PlayerData playerdata)  //임시코드입니다 실제로는 스텟 레벨을 가져올 예정
+    {
+        Stage = playerdata.Stage;
+        Gold = playerdata.Gold;
+        Attack = playerdata.Attack;
+        Critical = playerdata.Critical;
+        CriticalDmg = playerdata.Critical;
+        BonusGold = playerdata.BonusGold;
+
+        if (playerdata != null)
+            SaveDataToJSON.SaveUsers(playerdata);
+    }
+}
