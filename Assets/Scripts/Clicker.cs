3b6779980e0cb91f0d05ab9bd2ea6004a9638369
kimhyunjong77 <rlagus0407@gmail.com>
2025-06-12 17:28:01 +0900

    Add : 자동공격 추가
    클릭(미완), 자동공격 추가

---
+++ Assets/Scripts/Clicker.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Clicker : MonoBehaviour
+{
+    public bool autoAttackUnlocked = false;
+    public float autoAttackInterval = 5.0f; //자동공격 간격
+
+    private Coroutine autoAttackRoutine;
+
+    private void Start()
+    {
+        UnlockAutoClick();
+    }
+
+    //클릭시 공격
+    public void OnClickClickerButton()
+    {
+        //몬스터 체력 - 플레이어 최종데미지
+        //캐릭터 공격 애니매이션 동작
+        //공격 이펙트 동작 그리고 크리티컬시 다른 이펙트 동작
+        Debug.Log("클릭했습니다.");
+    }
+
+    //자동 공격 시작
+    public void AutoClick()
+    {
+        if (autoAttackUnlocked && autoAttackRoutine == null)
+        {
+            if (autoAttackUnlocked && autoAttackRoutine == null)
+            {
+                autoAttackRoutine = StartCoroutine(AutoClickRoutine());
+            }
+        }
+    }
+
+    //일정시간마다 클릭 코루틴
+    IEnumerator AutoClickRoutine()
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(autoAttackInterval);
+            OnClickClickerButton();
+        }
+    }
+
+    // 상점에서 구매시 자동클릭해금
+    public void UnlockAutoClick()
+    {
+        if (!autoAttackUnlocked)
+        {
+            autoAttackUnlocked = true;
+            AutoClick();
+        }
+    }
+}
