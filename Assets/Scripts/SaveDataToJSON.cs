4d9284eeddf8c7e1562ca3708d1fde868d9dc166
Hae1385 <206494390+Hae1385@users.noreply.github.com>
2025-06-16 09:38:51 +0900

    📝Docs : dataPath와 persistentDataPath 차이 주석추가

--- Assets/Scripts/SaveDataToJSON.cs
+++ Assets/Scripts/SaveDataToJSON.cs
@@ -3,7 +3,10 @@ using UnityEngine;
 
 public class SaveDataToJSON : MonoBehaviour
 {
-    private static string filePath => Path.Combine(Application.persistentDataPath, "UserData.json");
+    private static string filePath => Path.Combine(Application.dataPath, "JSON/UserData.json");
+    //Application.dataPath, "JSON/UserData.json" //에셋 위치 : 개발중에 사용하기 적합
+    //Application.persistentDataPath, "UserData.json"  //영구 저장 위치 : 빌드 할때 사용하기 적합
+    //나중에 제출할 때 영구저장위치로 변경 필요
 
     public static void SaveUsers(PlayerData playerData)
     {
