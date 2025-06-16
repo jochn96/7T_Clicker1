ce27525db4d0eee0b4534cf3e1ac71d3eeb41e8d
TenChoU <fgd5463@gmail.com>
2025-06-16 11:36:59 +0900

    FIX : 적 수정
    스프라이트 추가
    
    코드 수정 중

---
+++ Assets/Scripts/Enemy/Enemy.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    [SerializeField] 
+    private EnemyCenter enemyLife;
+
+    public EnemyCenter enemyCenter { set { enemyCenter =  value; } }
+
+    public void WatchEnemyInfo()
+    {
+        Debug.Log("적 체력::" + EnemyCenter.enemyLife);
+    }
+}
