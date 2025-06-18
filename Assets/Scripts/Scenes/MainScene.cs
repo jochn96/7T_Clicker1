11c5ba93985c61129e7eeb9f40a68e7b15c48dea
jochn96 <whtjdcks55@naver.com>
2025-06-18 16:38:42 +0900

    fix : 프리팹수정

--- Assets/Scripts/Scenes/MainScene.cs
+++ Assets/Scripts/Scenes/MainScene.cs
@@ -8,15 +8,15 @@ public class MainScene : MonoBehaviour
     void Start()
     {
         // Resources 폴더에서 UI_Player_Stat 프리팹을 로드하고 인스턴스화
-        GameObject playerStatUIPrefab = Resources.Load<GameObject>("UI/UI_Player_Canvas");
-        if (playerStatUIPrefab != null)
-        {
-            Instantiate(playerStatUIPrefab);
-        }
-        else
-        {
-            Debug.LogError("UI_Player_Stat 프리팹을 찾을 수 없습니다. Resources/UI/ 경로를 확인하세요.");
-        }
+        // GameObject playerStatUIPrefab = Resources.Load<GameObject>("UI/UI_Player_Canvas");
+        // if (playerStatUIPrefab != null)
+        // {
+        //     Instantiate(playerStatUIPrefab);
+        // }
+        // else
+        // {
+        //     Debug.LogError("UI_Player_Stat 프리팹을 찾을 수 없습니다. Resources/UI/ 경로를 확인하세요.");
+        // }
     }
 
   
